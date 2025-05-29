aa9212607b5b0cc969f1e573168fb86e40aa4816
Junior <junior.jr.jl@gmail.com>
2025-05-29 19:20:03 -0300

    fix: replace kotlin to java

---
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/AlgasensorsTemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.temperature.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgasensorsTemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgasensorsTemperatureMonitoringApplication.class, args);
+	}
+
+}
