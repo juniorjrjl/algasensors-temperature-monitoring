0acee5683fcc183fc85065a5eda53ce44061ec69
Junior <junior.jr.jl@gmail.com>
2025-06-01 18:30:47 -0300

    feat: configurações gradle e implementação de ids distribuidos

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+	idea
 	java
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
@@ -10,6 +11,13 @@ version = "0.0.1-SNAPSHOT"
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+idea{
+	module{
+		isDownloadJavadoc = true
+		isDownloadSources = true
 	}
 }
 
@@ -26,10 +34,16 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.apache.commons:commons-lang3:3.17.0")
+
 	compileOnly("org.projectlombok:lombok")
+
 	runtimeOnly("com.h2database:h2")
+
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
